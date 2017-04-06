d27a39a17d2d17eaa9f0a3978d2623693146e335
liufa <mataswork@gmail.com>
2017-04-06 13:05:01 +0700

    First version that pulls data with selenium

--- DellServiceTagSelenium/SeleniumDataScrapper.cs
+++ DellServiceTagSelenium/SeleniumDataScrapper.cs
@@ -1,9 +1,11 @@
 ﻿using DellServiceTagData;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DellServiceTagSelenium
@@ -16,18 +18,36 @@ namespace DellServiceTagSelenium
 
         public ChromeDriver Driver { get; private set; }
 
-        public IComClassDellAsset GetDellAsset(string assetId)
+        public IComClassDellAsset GetDellAsset(string serviceTag)
         {
-            this.Driver.Url = $"http://www.dell.com/support/home/us/en/19/product-support/servicetag/{assetId}/diagnose";
+            this.Driver.Url = $"http://www.dell.com/support/home/us/en/19/product-support/servicetag/{serviceTag}/diagnose";
             this.Driver.Navigate();
-            var feedbackOverlay = this.Driver.FindElementsByCssSelector("[onclick='ipe120994.clWin();']'");
+            var feedbackOverlay = this.Driver.FindElementsByCssSelector("[onclick='ipe120994.clWin();']");
             if(feedbackOverlay.Any())
                 feedbackOverlay.First().Click();
 
             var configurationTab = this.Driver.FindElementsById("tab-configuration");
             configurationTab.First().Click();
+            Thread.Sleep(3000);
+            var configurationDivContainer = this.Driver.FindElementsById("subSectionA").First();
 
-            return null;
+            var rows = configurationDivContainer.FindElements(By.CssSelector("tr"));
+            var computerModelRow = rows.Where(o => o.Text.Contains("Computer Model")).FirstOrDefault();
+            var computerModel = computerModelRow.FindElements(By.CssSelector("td"))[1].Text;
+            var shippingDateRow = rows.Where(o => o.Text.Contains("Shipping Date")).FirstOrDefault();
+            var shippingDateCell = shippingDateRow.FindElements(By.CssSelector("td"))[1].Text;
+
+            var shippingDate = DateTime.ParseExact(shippingDateCell, new[] { "M/d/yyyy", "M/dd/yyyy", "MM/dd/yyyy" }, null, System.Globalization.DateTimeStyles.RoundtripKind);
+
+            var countryRow = rows.Where(o => o.Text.Contains("Country")).FirstOrDefault();
+            var country = countryRow.FindElements(By.CssSelector("td"))[1].Text;
+
+            return new DellAsset{
+                ServiceTag = serviceTag,
+                MachineDescription = computerModel,
+                ShipDate = shippingDate.ToString("yyyy-MM-dd"),
+                CountryLookupCode = country
+            };
         }
     }
 }
